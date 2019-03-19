31b82c6790cfc8c81243467fbd4f9966eddc4bec
Diego Alberto Costa <d.costa1990@outlook.com>
2019-03-19 18:45:36 -0300

    Add files via upload
    First Commit.. React practice

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id : 1,
+        name: 'Pipa Seni',
+        username: 'Guido',
+        email: 'soypipanoguido@gmail.com'
+    },
+    {
+        id : 2,
+        name: 'Facundo Domenichini',
+        username: 'El Negro',
+        email: 'Domayor@gmail.com'
+    },
+    {
+        id : 3,
+        name: 'Sergio Landolfo',
+        username: 'Nicolas',
+        email: 'tenso@gmail.com'
+    },
+    {
+        id : 4,
+        name: 'Ignacio Rausch',
+        username: 'Nachito',
+        email: 'troll@gmail.com'
+    },
+    {
+        id : 5,
+        name: 'Gabriel Pietracupa',
+        username: 'Gabote',
+        email: 'elmago@gmail.com'
+    },
+    {
+        id : 6,
+        name: 'Agustin Cappuccio',
+        username: 'elAgus',
+        email: 'elcebra@gmail.com'
+    },
+    {
+        id : 7,
+        name: 'Matias Cardinale',
+        username: 'Cardo',
+        email: 'arqueria@gmail.com'
+    },
+    {
+        id : 8,
+        name: 'Diego Costa',
+        username: 'Reverendo',
+        email: 'd.costa1990@gmail.com'
+    },
+];
